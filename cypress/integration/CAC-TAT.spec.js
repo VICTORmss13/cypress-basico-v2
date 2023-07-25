29fa5697412bd49d4b19148ae3dc9c20aab307f9
Victor Mendes <victormendes13@gmail.com>
2023-07-25 01:01:22 -0300

    Late night exercises4

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,13 +9,42 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Teste, teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,'
         cy.get('#firstName').type('Walmyr')
         cy.get('#lastName').type('Filho')
         cy.get('#email').type('walmyr@exemplo.com')
-        cy.get('#open-text-area').type('Teste')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
     })
-  })
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+        cy.get('#firstName').type('Walmyr')
+        cy.get('#lastName').type('Filho')
+        cy.get('#email').type('walmyr@exemplo,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function () {
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
+    })
+
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+        cy.get('#firstName').type('Walmyr')
+        cy.get('#lastName').type('Filho')
+        cy.get('#email').type('walmyr@exemplo.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+
+})
