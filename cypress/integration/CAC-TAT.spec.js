80803febf96fbf5c82aae61b322364e910c9b45f
Victor Mendes <victormendes13@gmail.com>
2023-09-23 18:54:07 -0300

    Teste com info Victor

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,18 +11,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('preenche os campos obrigatórios e envia o formulário', function() {
         const longText = 'Teste, teste,teste,teste,teste,teste,teste,teste,teste,teste,teste,'
-        cy.get('#firstName').type('Walmyr')
-        cy.get('#lastName').type('Filho')
-        cy.get('#email').type('walmyr@exemplo.com')
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Mendes')
+        cy.get('#email').type('victormendes@exemplo.com')
         cy.get('#open-text-area').type(longText, { delay: 0 })
         cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
-        cy.get('#firstName').type('Walmyr')
-        cy.get('#lastName').type('Filho')
-        cy.get('#email').type('walmyr@exemplo,com')
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Mendes')
+        cy.get('#email').type('victormendes@exemplo,com')
         cy.get('#open-text-area').type('Teste')
         cy.contains('button', 'Enviar').click()
 
@@ -36,9 +36,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
-        cy.get('#firstName').type('Walmyr')
-        cy.get('#lastName').type('Filho')
-        cy.get('#email').type('walmyr@exemplo.com')
+        cy.get('#firstName').type('Victor')
+        cy.get('#lastName').type('Mendes')
+        cy.get('#email').type('victormendes@exemplo.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Teste')
         cy.contains('button', 'Enviar').click()
@@ -48,20 +48,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('Prrenche e limpa os campos nome, sobrenome, email e telefone', function (){
         cy.get('#firstName')
-            .type('Walmyr')
-            .should('have.value', 'Walmyr')
+            .type('Victor')
+            .should('have.value', 'Victor')
             .clear()
             .should('have.value', '')
 
         cy.get('#lastName')
-            .type('Filho')
-            .should('have.value', 'Filho')
+            .type('Mendes')
+            .should('have.value', 'Mendes')
             .clear()
             .should('have.value', '')    
     
         cy.get('#email')
-            .type('walmyr@exemplo.com')
-            .should('have.value', 'walmyr@exemplo.com')
+            .type('victormendes@exemplo.com')
+            .should('have.value', 'victormendes@exemplo.com')
             .clear()
             .should('have.value', '')
         cy.get('#phone')
